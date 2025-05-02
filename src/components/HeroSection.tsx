769816cb696e184f63ba9e04390356ae42e39c95
sharon17kevin <sharon17kevin@gmail.com>
2025-05-02 23:16:37 +0100

    Adjust the components in the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -47,31 +47,32 @@ const HeroSection = () => {
   return (
     <section
       ref={containerRef}
-      className="relative h-screen bg-primary md:h-[300vh]"
+      className="relative h-[800px] bg-primary sm:h-[300vh]"
     >
       <div
         style={{
           backgroundImage:
-            'url("https://images.unsplash.com/photo-1509023464722-18d996393ca8?auto=format&fit=crop&q=80")',
+            'url("https://images.unsplash.com/photo-1511497584788-876760111969?auto=format&fit=crop&q=80")',
+            // "https://images.unsplash.com/photo-1509023464722-18d996393ca8?auto=format&fit=crop&q=80"
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
         className="sticky top-0 w-full min-h-full md:min-h-screen flex flex-col text-black bg-white"
       >
         <div className="absolute inset-0 bg-white/90 flex flex-col w-full h-full">
-          <div className=" flex-col md:flex-row flex w-[1400px] max-w-full h-full  mx-auto items-center justify-center">
-            <div className="  w-full lg:w-1/2 md:w-3/5 flex flex-col h-full justify-start overflow-visible">
-              <div className="flex flex-1 font-cormorantUpright font-semibold text-2xl sm:text-6xl md:text-[80px] overflow-visible items-center justify-center md:justify-start">
+          <div className=" flex-col md:flex-row flex w-[1400px] max-w-full h-full  mx-auto items-center justify-evenly">
+            <div className="  w-full lg:w-1/2 md:w-3/5 flex flex-col h-auto md:h-full justify-start overflow-visible">
+              <div className="flex flex-1 font-cormorantUpright font-semibold text-5xl md:text-[80px] overflow-visible items-center justify-center md:justify-start">
                 <p className="text-customGreen-dark ml-10 text-center md:text-left ">
                   HEY, I'M <br />
                   SHARON-KEVIN
                 </p>
               </div>
-              <div className=" hidden md:flex flex-1 overflow-visible">
+              <div className="hidden md:flex flex-1 overflow-visible">
                 <RiveComponent />
               </div>
             </div>
-            <div className="  h-full w-full lg:w-1/2 md:w-2/5 flex flex-col justify-end items-center">
+            <div className=" h-auto p-2 md:h-full w-full lg:w-1/2 md:w-2/5 flex flex-col justify-end items-center">
               {/* <div className="max-w-[600px] max-h-[550px]">
                 <img
                   src={photoCard}
@@ -91,7 +92,7 @@ const HeroSection = () => {
                 </div>
               </div>
               <div className=" w-full h-2/6 flex justify-center items-end pb-3 md:pb-0 md:items-center">
-                <div className="h-auto m-2 md:h-24 w-full lg:w-8/12 px-2 py-2 bg-primary rounded-lg flex justify-around items-center">
+                <div className="h-auto m-2 mt-6 md:h-24 md:mt-0 w-full lg:w-8/12 px-2 py-2 bg-primary rounded-lg flex justify-around items-center">
                   <div className="border-r border-black flex justify-center flex-1">
                     <a href="https://x.com/Sharonkvn">
                       {<FaTwitter
